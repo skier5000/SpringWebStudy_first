a9a13279d097a9e6c3ef22cafaeaa57ab987ea89
Lee JunBeon <skier5000@naver.com>
2021-06-09 17:29:40 +0900

    HomeController/home.html 추가

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+
+    /**
+     * static 의 index.html 웰컴페이지를 무시 → / 로 매핑된게 있으므로
+     */
     @GetMapping("/")
     public String home(){
-        
+
         return "home";
     }
 
