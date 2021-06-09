7cda9223098f0648b360505e136f88e268375b12
Lee JunBeon <skier5000@naver.com>
2021-06-09 16:53:18 +0900

    자바 코드로 직접 스프링빈 등록 방법 주석 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 자바 코드로 직접 스프링빈을 등록하는 방법
+ * 정형화되지 않거나, 상황에 따라 구현 클래스를 변경해야하면 설정을 토앻 스프링빈으로 등록
  */
 @Configuration
 public class SpringConfig {
