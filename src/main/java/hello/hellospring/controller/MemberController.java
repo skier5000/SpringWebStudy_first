6c78035cd193a09a0c2b7343fb0bc950381764ff
Lee JunBeon <skier5000@naver.com>
2021-06-16 20:07:07 +0900

    회원가입 껍데기 추가

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -21,6 +22,12 @@ public class MemberController {
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String CreateForm(){
+        return "members/createMemberForm";
+    }
+
 
 
 }
