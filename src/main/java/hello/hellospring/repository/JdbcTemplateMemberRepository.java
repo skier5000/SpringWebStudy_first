87109fa4b66c2147b65a1eb9cbc05460ea993781
Lee JunBeon <skier5000@naver.com>
2021-06-18 14:24:33 +0900

    JdbcTemplateMemberRepository 추가

--- src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -1,9 +1,13 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,7 +15,8 @@ public class JdbcTemplateMemberRepository implements MemberRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
-    //
+    // 반복코드를 제거해주는 JdbcTemplate (실무에서도 사용)
+    @Autowired
     public JdbcTemplateMemberRepository(DataSource dataSource){
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
@@ -23,7 +28,9 @@ public class JdbcTemplateMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return jdbcTemplate.query("select * from member where id = ?", memberRowMapper());
+
+        //return Optional.empty();
     }
 
     @Override
@@ -34,5 +41,15 @@ public class JdbcTemplateMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return null;
+    }
+
+    // 결과가 나오는걸 RowMapper 로 매핑을 해주어야함 그래서 RowMapper 메소드 만들기
+    private RowMapper<Member> memberRowMapper(){
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setId(rs.getLong("id"));
+            member.setName(rs.getString("name"));
+            return member;
+        };
     }
 }
